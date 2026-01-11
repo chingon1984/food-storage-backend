83a20ad8652552ed64427922c2e1f27dd4c4f66f
Patrick <p.hass1984@gmail.com>
2026-01-11 01:12:50 +0100

    added start spring project

---
+++ src/main/java/com/chingon/FoodStorageApp/FoodStorageAppApplication.java
@@ -0,0 +1,13 @@
+package com.chingon.FoodStorageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodStorageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodStorageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chingon/FoodStorageApp/FoodStorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chingon.FoodStorageApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStorageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
