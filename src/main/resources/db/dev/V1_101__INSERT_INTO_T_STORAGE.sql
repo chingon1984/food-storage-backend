14e4bcb2c28293a5bdf6f56ba152fa924a2b584b
Patrick Hasselbach <patrick.hasselbach@protonmail.com>
2026-05-13 23:32:55 +0200

    added test data to storages and container

---
+++ src/main/resources/db/dev/V1_101__INSERT_INTO_T_STORAGE.sql
@@ -0,0 +1,10 @@
+INSERT INTO T_STORAGE(name, user_id, description, archived)
+VALUES
+    ('Storage_living-room', 1, 'Storage at the living room', false),
+    ('Storage_garage', 1, 'Storage at the garage', false),
+    ('Storage_basement', 1, 'Storage at the basement', false),
+    ('Storage_sleeping-room', 2, 'Storage at the sleeping room', false),
+    ('Storage_secret-room', 2, 'Storage at the secret room', false),
+    ('Storage_garden', 2, 'Storage at the garden', true),
+    ('Storage_roof', 1, 'Storage at the roof', true);
+
---
+++ src/main/resources/db/dev/V1_102__INSERT_INTO_T_CONTAINER.sql
@@ -0,0 +1,9 @@
+INSERT INTO T_CONTAINER(name, storage_id, description, archived)
+VALUES
+    ('Container_Fruits-Vegetables', 1, 'Container for fruits and vegetables', false),
+    ('Container_Meat-Fish', 1, 'Container for meat and fish', false),
+    ('Container_Ready2Serve', 2, 'Container for ready to serve meals', false),
+    ('Container_Ready2Serve', 4, 'Container for ready to serve meals', false),
+    ('Container_Coffee_Tea', 3, 'Container for coffee and tea', false),
+    ('Container_Nutrients', 3, 'Container for coffee and tea', false),
+    ('Container_Gold', 5, 'Container for gold', false);
