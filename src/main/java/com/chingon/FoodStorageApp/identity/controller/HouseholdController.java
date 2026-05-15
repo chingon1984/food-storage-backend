5df853189dd803222e5dabc60cf70e1d3afba6c8
Patrick Hasselbach <patrick.hasselbach@protonmail.com>
2026-05-15 14:15:01 +0200

    fixed bug

--- src/main/java/com/chingon/FoodStorageApp/identity/controller/HouseholdController.java
+++ src/main/java/com/chingon/FoodStorageApp/identity/controller/HouseholdController.java
@@ -5,15 +5,14 @@ import com.chingon.FoodStorageApp.identity.dto.HouseholdResponse;
 import com.chingon.FoodStorageApp.identity.service.IHouseholdService;
 import com.chingon.FoodStorageApp.shared.annotation.HouseholdRequestBody;
 import com.chingon.FoodStorageApp.shared.constants.Routes;
-import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,30 +24,13 @@ public class HouseholdController {
 
     private final IHouseholdService householdService;
 
-    //    @GetMapping()
-    //    public ResponseEntity<List<HouseholdResponse>> getAllHouseholds() {
-    //        List<HouseholdResponse> householdResponses = householdService.getAllHouseholds();
-    //
-    //        return ResponseEntity.ok(householdResponses);
-    //    }
-
     @PostMapping()
-    public ResponseEntity<HouseholdResponse> createHousehold(
-            @Valid @RequestBody HouseholdRequest householdRequest) {
-        System.out.println(householdRequest.name());
+    public ResponseEntity<HouseholdResponse> createHousehold(@Valid @RequestBody @HouseholdRequestBody HouseholdRequest householdRequest) {
         HouseholdResponse householdResponse = householdService.createHousehold(householdRequest);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(householdResponse);
     }
-
-    @PostMapping("/hi")
-    public ResponseEntity<HouseholdRequest> test(@RequestBody HouseholdRequest householdRequest) {
-        System.out.println(householdRequest);
-
-        return ResponseEntity.ok(householdRequest);
-    }
-
 
 }
